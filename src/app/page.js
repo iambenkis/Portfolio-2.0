2158ff90ea662ea95e1ecc9e6e30989c96b0f21b
benkis <benkisenge03@gmail.com>
2023-08-08 12:19:31 +0200

    Update the main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 // import NavLinks from '@/components/NavLink'
 import Navbar from '@/components/Navbar'
 import Header from '@/pages/Home'
+import Projects from '@/components/Projects'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <div className="px-80 py-10">
         <Header />
+        <Projects />
       </div>
     </main>
   )
