6010dd73ccac5c9b2183ceb021211931189f575c
benkis <benkisenge03@gmail.com>
2023-08-08 16:38:00 +0200

    Create the life updates object

---
+++ src/data/lifeProgress/index.jsx
@@ -0,0 +1,22 @@
+export const lifeupdates = [
+  {
+    year: '2021',
+    title: 'Started my first job',
+    description: 'I started my first job as a Software Engineer at a startup.',
+  },
+  {
+    year: '2021',
+    title: 'Started my first job',
+    description: 'I started my first job as a Software Engineer at a startup.',
+  },
+  {
+    year: '2021',
+    title: 'Started my first job',
+    description: 'I started my first job as a Software Engineer at a startup.',
+  },
+  {
+    year: '2021',
+    title: 'Started my first job',
+    description: 'I started my first job as a Software Engineer at a startup.',
+  },
+]
