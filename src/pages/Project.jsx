b774b1e86c3ab36897b32ee6d8963529c3224b64
benkis <benkisenge03@gmail.com>
2023-08-09 12:21:48 +0200

    Update project pages

---
+++ src/pages/Project.jsx
@@ -0,0 +1,12 @@
+// import Projects from '@/components/Projects'
+import Projects from '@/components/Projects'
+
+const Project = () => {
+  return (
+    <div>
+      <Projects />
+    </div>
+  )
+}
+
+export default Project
