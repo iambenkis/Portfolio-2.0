7d987880b1ce3414bd682597323a359e9f0699fd
benkis <benkisenge03@gmail.com>
2023-08-10 21:26:16 +0200

    Slice the project list

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -3,13 +3,14 @@ import { UpcommingProjects } from '@/data/projects'
 import ProjectItems from '@/data/projects'
 
 export default function Projects() {
+  const toDisplay = 4
   return (
     <section className="mt-10">
       <div>
-        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
+        <h3 className="font-bold text-[#172b4d] text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
           Projects
         </h3>
-        <ProjectItems projects={projects} />
+        <ProjectItems projects={projects} toDisplay={toDisplay} />
         <a
           type="button"
           class="flex justify-center items-center text-sm my-4 mx-auto px-4 py-2 rounded-md font-medium text-gray-900 dark:text-gray-100"
@@ -33,7 +34,7 @@ export default function Projects() {
         </a>
       </div>
       <div className="mt-4">
-        <h3 class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
+        <h3 class="font-bold text-2xl text-[#172b4d] md:text-4xl tracking-tight mb-4 text-black dark:text-white">
           Upcomming Projects
         </h3>
         <ProjectItems projects={UpcommingProjects} />
