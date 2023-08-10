a4196cc376104be2e4bf898d6af00135163347ea
benkis <benkisenge03@gmail.com>
2023-08-10 21:27:19 +0200

    Add projects to the array

--- src/data/projects/index.jsx
+++ src/data/projects/index.jsx
@@ -48,6 +48,21 @@ export const projects = [
     live: 'https://google.com',
     source: 'https://github.com',
   },
+  {
+    name: 'Project 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptatum.',
+    stacks: [
+      'React',
+      'Next.js',
+      'TailwindCSS',
+      'React',
+      'Next.js',
+      'TailwindCSS',
+    ],
+    live: 'https://google.com',
+    source: 'https://github.com',
+  },
 ]
 
 export const UpcommingProjects = [
@@ -87,27 +102,29 @@ export const UpcommingProjects = [
 export default function ProjectItems(props) {
   return (
     <div className="grid grid-cols-2 gap-4">
-      {props.projects.map((project, index) => (
+      {props.projects.slice(0, props.toDisplay).map((project, index) => (
         <a
           key={index}
           href={project.live}
           target="_blaket"
-          className="border-solid border-[1px] border-[#b5b5b5] rounded-xl shadow-lg p-4"
+          className="p-[.7px] bg-gradient-to-r from-blue-400/50 to-transparent rounded-xl shadow-lg  hover:bg-gradient-to-r from-blue-200/50 to-yellow-200/25 transition duration-500"
         >
-          <IoFastFood className="text-black-300 text-3xl mx-2 my-4" />
-          <h3 className="font-bold">{project.name}</h3>
-          <p className="text-[#374151] my-2">{project.description}</p>
-          <ul className="flex flex-wrap mt-4">
-            {project.stacks &&
-              project.stacks.map((stack, index) => (
-                <li
-                  key={index}
-                  className=" italic leading-5 mb-2 dark:border dark:border-zinc-700 text-gray-700 dark:text-gray-300 dark:bg-transparent rounded-md text-xs bg-gray-100  mr-2 px-1"
-                >
-                  {stack}
-                </li>
-              ))}
-          </ul>
+          <div className="p-2 bg-neutral-100 rounded-xl w-full h-full hover:bg-gradient-to-r from-blue-200/50 to-yellow-200/25 transition duration-500">
+            <IoFastFood className="text-[#172b4d] text-3xl mx-2 my-4" />
+            <h3 className="font-bold">{project.name}</h3>
+            <p className="text-[#374151] my-2">{project.description}</p>
+            <ul className="flex flex-wrap mt-4">
+              {project.stacks &&
+                project.stacks.map((stack, index) => (
+                  <li
+                    key={index}
+                    className=" italic leading-5 mb-2 dark:border dark:border-zinc-700 text-gray-700 dark:text-gray-300 dark:bg-transparent rounded-md text-xs bg-gray-100  mr-2 px-1"
+                  >
+                    {stack}
+                  </li>
+                ))}
+            </ul>
+          </div>
         </a>
       ))}
     </div>
