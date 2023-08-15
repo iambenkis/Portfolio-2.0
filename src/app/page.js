eeedc04b40f05dcfd76d7f0c66c0a9cb9c4174e8
benkis <benkisenge03@gmail.com>
2023-08-15 18:57:21 +0200

    Update url in app page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
-import Image from 'next/image'
-// import NavLinks from '@/components/NavLink'
-import Navbar from '@/components/Navbar'
-import Header from '@/pages/Home'
-import Projects from '@/components/Projects'
-import LifeUpdate from '@/components/LifeUpdate'
-import Whatsapp from '@/components/Whatsapp'
-import Footer from '@/components/Footer'
+import Header from '../pages/Home'
+import Projects from '../components/Projects'
+import LifeUpdate from '../components/LifeUpdate'
+import Whatsapp from '../components/Whatsapp'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-neutral-100">
+    <main className="flex min-h-screen flex-col items-center bg-neutral-100 bg-gray-900">
       <div className="px-80 pb-10 pt-5 mt-40">
         <Header />
         <Projects />
