2295e9a2f9f44fe32381cdf41cfb1dddd536efbc
benkis <benkisenge03@gmail.com>
2023-08-18 22:16:24 +0200

    Add mobile version on the Project component

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Projects() {
   const toDisplay = 4
   return (
-    <section className="mt-10">
+    <section className="px-7 md:px-0 mt-10">
       <div>
         <h3 className="font-bold text-[#172b4d] text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
           Projects
@@ -14,7 +14,7 @@ export default function Projects() {
         <ProjectItems projects={projects} toDisplay={toDisplay} />
         <Link
           type="button"
-          class="flex justify-center items-center text-sm my-4 mx-auto px-4 py-2 rounded-md font-medium text-gray-900 dark:text-gray-100"
+          class="flex justify-center items-center text-sm my-7 mx-auto px-4 py-2 rounded-md font-medium text-gray-900 dark:text-gray-100"
           href="/projects"
         >
           See More
