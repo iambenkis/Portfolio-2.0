7df19d7fa640feca53df71b0f4220c63f908f1b4
benkis <benkisenge03@gmail.com>
2023-08-18 22:16:01 +0200

    Add mobile version on the  navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -15,15 +15,16 @@ export default function Navbar() {
     }
   }
   return (
-    <div className="fixed z-20 flex items-center justify-between w-full px-60 py-7 backdrop-blur-md bg-black/55 ">
-      <div
-        className="cursor-pointer border px-4 py-2 border-solid border-[1px] border-[#b5b5b5] rounded-full shadow-lg mr-10"
-        onClick={toggleTheme}
-      >
-        {theme === 'light' ? <MdOutlineLightMode /> : <BiSolidMoon />}
-      </div>
-      <NavLinks />
-    </div>
+    // <div className="none fixed z-20 flex items-center justify-between w-full sm:px-60 py-7 backdrop-blur-md bg-black/55 ">
+    //   <div
+    //     className="cursor-pointer border px-4 py-2 border-solid border-[1px] border-[#b5b5b5] rounded-full shadow-lg mr-10"
+    //     onClick={toggleTheme}
+    //   >
+    //     {theme === 'light' ? <MdOutlineLightMode /> : <BiSolidMoon />}
+    //   </div>
+    //   <NavLinks />
+    // </div>
+    <></>
   )
 }
 
