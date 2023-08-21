a719fbba48de900787776e378b8f16dc085d6532
benkis <benkisenge03@gmail.com>
2023-08-21 18:49:58 +0200

    Update navbar dark mode

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -36,52 +36,67 @@ export default function Navbar() {
   // }, [])
   return (
     <div
-      className={`fixed z-20  flex items-center justify-between w-full px-7 sm:px-60 py-7 backdrop-blur-md ${
-        theme === 'dark' ? 'bg-gray-800 text-[#fff]' : 'bg-gray-100/50'
-      }  `}
+      className={`fixed z-20  flex items-center justify-between w-full px-7 sm:px-60 py-7 backdrop-blur-md bg-gray-100/50  dark:bg-gray-900`}
     >
       <div
         className=" cursor-pointer border px-4 py-2 border-solid border-[1px] border-[#b5b5b5] rounded-full shadow-lg mr-10"
         onClick={toggleTheme}
       >
-        {theme === 'light' ? <MdOutlineLightMode /> : <BiSolidMoon />}
+        {theme === 'light' ? (
+          <BiSolidMoon className="dark:text-white" />
+        ) : (
+          <MdOutlineLightMode className="dark:text-white" />
+        )}
       </div>
       <div className="nav-bar">
         <NavLinks />
       </div>
       <div className="mobile-nav">
         <ul className="flex items-center">
-          <li>
+          <li className="dark:text-white">
             <Link href="/">Home</Link>
           </li>
-          <li className="mx-4">
+          <li className="mx-4 dark:text-white">
             <Link href="/projects">Projects</Link>
           </li>
-          <li className="p-2 bg-gray-300 rounder" onClick={toggleMenu}>
+          <li
+            className="p-2 bg-gray-300 dark:bg-gray-700 rounder"
+            onClick={toggleMenu}
+          >
             {' '}
-            <FiMenu />{' '}
+            <FiMenu className="dark:text-white" />{' '}
           </li>
         </ul>
         <div
           className={`absolute ${
             !show ? 'hidden' : ''
-          }  top-[5rem] left-[2.5%] w-[95%] rounded bg-neutral-100 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]`}
+          }  top-[5rem] left-[2.5%] w-[95%] rounded bg-neutral-100 dark:bg-gray-800 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]`}
         >
           <ul className="px-2">
             <li className="py-4 border-t-0 font-bold px-4">
-              <Link href="/blog">Blog</Link>
+              <Link href="/blog" className="text-black dark:text-white">
+                Blog
+              </Link>
             </li>
             <li className="py-4 border-t font-bold px-4">
-              <Link href="/about">About</Link>
+              <Link href="/about" className="text-black dark:text-white">
+                About
+              </Link>
             </li>
             <li className="py-4 border-t font-bold px-4">
-              <Link href="/contact">Contact</Link>
+              <Link href="/contact" className="text-black dark:text-white">
+                Contact
+              </Link>
             </li>
             <li className="py-4 border-t font-bold px-4">
-              <Link href="/dashbord">Dashbord</Link>
+              <Link href="/dashbord" className="text-black dark:text-white">
+                Dashbord
+              </Link>
             </li>
             <li className="py-4 border-t font-bold px-4">
-              <Link href="/testimonies">Testimonies</Link>
+              <Link href="/testimonies" className="text-black dark:text-white">
+                Testimonies
+              </Link>
             </li>
           </ul>
         </div>
