64669bece8e62489da51b47601b1a27a392ad30d
benkis <benkisenge03@gmail.com>
2023-08-21 18:48:13 +0200

    Update skills section dark mode

--- src/app/about/skills/index.jsx
+++ src/app/about/skills/index.jsx
@@ -11,10 +11,13 @@ export default function Skills() {
         </h3>
         <div className="flex flex-col md:flex-row">
           <div className="md:w-[60%] w-full mr-7 ">
-            <h4 className="font-bold  text-[30px] mb-4 uppercase">
+            <h4 className="font-bold  text-[30px] mb-4 uppercase dark:text-gray-500 ">
               MY MISSION IS
-              <span className="text-[#172b4d]"> DESIGN & DEVELOP</span> THE BEST
-              WEBSITES AROUND
+              <span className="text-[#172b4d] dark:text-white">
+                {' '}
+                DESIGN & DEVELOP
+              </span>{' '}
+              THE BEST WEBSITES AROUND
             </h4>
             <p className="text-gray-400 text-justify mt-4 md:mt-0">
               I can help you build a product , feature or website Look through
@@ -23,7 +26,9 @@ export default function Skills() {
             </p>
           </div>
           <div>
-            <h4 className="font-bold text-[#172b4d] text-[15px] ">Languages</h4>
+            <h4 className="font-bold pt-2 text-[#172b4d] text-[15px] dark:text-white">
+              Languages
+            </h4>
             <ul className="flex w-full mt-3 mb-4">
               <li className="bg-gray-500/25 rounded border w-[50px] h-[50px] flex justify-center items-center mr-3">
                 <DiRuby className="text-red-600 text-2xl" />
@@ -35,7 +40,7 @@ export default function Skills() {
                 <TbBrandCpp className="text-blue-600 text-2xl" />
               </li>
             </ul>
-            <h4 className="font-bold text-[#172b4d] text-[15px]">
+            <h4 className="font-bold text-[#172b4d] text-[15px] dark:text-white">
               Libraries && Frameworks && Tools
             </h4>
             <ul className="flex w-full mt-3 mb-4 p-1">
