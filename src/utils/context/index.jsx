725962d089a2e804f10502d0ee754e774b1f912c
benkis <benkisenge03@gmail.com>
2023-08-22 01:39:21 +0200

    Add localeStorage on darkmode state

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,13 +1,19 @@
 'use client'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(
+    localStorage.getItem('Mode') === 'light' ? 'light' : 'dark',
+  )
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light')
   }
+
+  useEffect(() => {
+    localStorage.setItem('Mode', theme)
+  }, [theme])
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
