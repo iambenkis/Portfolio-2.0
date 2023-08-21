0cbd282fd921cdd625ecfccfc4550a101c17b350
benkis <benkisenge03@gmail.com>
2023-08-21 18:49:12 +0200

    Update project page dark mode

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -9,7 +9,7 @@ import { VscGithub } from 'react-icons/vsc'
 const Project = () => {
   // const { theme } = useContext(ThemeContext)
   return (
-    <div className="bg-neutral-100 py-10 md:px-80">
+    <div className="bg-neutral-100 dark:bg-gray-900 py-10 md:px-80">
       <div className="pb-10 px-7 pt-10 mt-10">
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
           Projects
@@ -19,7 +19,10 @@ const Project = () => {
           passions, spanning commercial ventures and deeper explorations,
           revealing my multifaceted development skills. Get in touch for Coffee
           Chat on{' '}
-          <a class="font-bold text-black-500 hover:underline" href="/blog">
+          <a
+            class="font-bold text-black-500 dark:text-white hover:underline"
+            href="/blog"
+          >
             here
           </a>{' '}
         </h2>
