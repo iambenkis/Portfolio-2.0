c3b9de3e6b9ad7b56d36dfe160ec22452e5263a2
benkis <benkisenge03@gmail.com>
2023-08-21 18:48:29 +0200

    Update blog page dark mode

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -21,19 +21,22 @@ const blogs = [
 
 export default function Blog() {
   return (
-    <div className="px-7 md:px-80 pt-20 pb-10 bg-neutral-100">
-      <h2 className="mt-10 font-bold text-[#172b4d] text-2xl md:text-4xl mb-4">
+    <div className="px-7 md:px-80 pt-20 pb-10 bg-neutral-100 dark:bg-gray-900">
+      <h2 className="mt-10 font-bold text-[#172b4d] text-2xl md:text-4xl mb-4 dark:text-white">
         Articles I have written recently
       </h2>
       <div className="px-0 md:px-5">
         {blogs.map((blog) => (
           <div
             key={blog.description}
-            className="bg-[#eeeeeea2] my-4 px-2 md:px-4 py-2 rounded border"
+            className="bg-[#eeeeeea2] dark:bg-gray-900 my-4 px-2 md:px-4 py-2 rounded border"
           >
-            <h2 className="font-bold">{blog.title}</h2>
+            <h2 className="font-bold dark:text-white">{blog.title}</h2>
             <p className="text-gray-500">{blog.description}</p>
-            <a href={blog.link} className="text-blue-600/50 italic">
+            <a
+              href={blog.link}
+              className="text-blue-600/50 dark:text-blue-500 italic"
+            >
               Read more
             </a>
           </div>
