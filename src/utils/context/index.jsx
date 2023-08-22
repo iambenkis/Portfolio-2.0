b3b087f19739b534b79bce778efd02cf710380ac
benkis <benkisenge03@gmail.com>
2023-08-22 15:57:48 +0200

    Fix localstorage undefined bug

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -4,15 +4,18 @@ import { createContext, useState, useEffect } from 'react'
 export const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(
-    localStorage.getItem('Mode') === 'light' ? 'light' : 'dark',
-  )
+  const [theme, setTheme] = useState(null)
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
+    setTheme(localStorage.getItem('Mode') === 'light' ? 'dark' : 'light')
+    localStorage.setItem('Mode', theme === 'light' ? 'dark' : 'light')
   }
 
   useEffect(() => {
-    localStorage.setItem('Mode', theme)
+    if (localStorage.getItem('Mode')) {
+      setTheme(localStorage.getItem('Mode'))
+    } else {
+      // setTheme(null)
+    }
   }, [theme])
 
   return (
