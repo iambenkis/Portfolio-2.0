bd65692314f876b8c3b95a531633579cf5e5fc65
benkis <benkisenge03@gmail.com>
2023-08-24 23:34:18 +0200

    Add life updates

--- src/data/lifeProgress/index.jsx
+++ src/data/lifeProgress/index.jsx
@@ -1,22 +1,131 @@
 export const lifeupdates = [
   {
-    year: '2021',
-    title: 'Started my first job',
-    description: 'I started my first job as a Software Engineer at a startup.',
+    year: '2023',
+
+    description: [
+      {
+        text: 'I started my first job as a Software Engineer at a startup.',
+        title: 'Full-Stack Applications 🚀',
+      },
+      {
+        text: 'I started my first job as a Software Engineer at a startup.',
+        title: 'Graduated from Microverse 🎓',
+      },
+    ],
+  },
+  {
+    year: '2022',
+
+    description: [
+      {
+        text: 'I started my first job as a Software Engineer at a startup.',
+        title: 'Promoted as a Code Reviewer',
+      },
+      {
+        text: 'I started my first job as a Software Engineer at a startup.',
+        title: 'Graduated from University 🎓',
+      },
+      {
+        text: 'I started my first job as a Software Engineer at a startup.',
+        title: 'Mentor web developer students at Microverse 🥳',
+      },
+    ],
   },
   {
     year: '2021',
-    title: 'Started my first job',
-    description: 'I started my first job as a Software Engineer at a startup.',
+
+    description: [
+      {
+        text: 'I got an A2 diploma in Electronic with 65%. Again, a big achievement for me.',
+        title: 'Introduced to web development 📚',
+      },
+      {
+        text: 'Be a part of a team of 6 engineers in the design and development of IoT solutions for industrial automation',
+        title: 'Integrate RLCG company',
+      },
+      {
+        text: 'We develop a fish farming system that allows the farmer to monitor the water quality and the fish health in real time. The system is composed of a mobile application, a web application and a hardware part.',
+        title: 'Competitive Innovation 🏆',
+      },
+    ],
   },
   {
-    year: '2021',
-    title: 'Started my first job',
-    description: 'I started my first job as a Software Engineer at a startup.',
+    year: '2020',
+
+    description: [
+      {
+        text: 'I have ingeniously crafted a captivating woodland using HTML for the trees, CSS for their vibrant Facebook-blue foliage, and JavaScript to infuse the very essence of social interactivity among the digital groves',
+        title: 'Clone of Facebook 💯',
+      },
+    ],
   },
   {
-    year: '2021',
-    title: 'Started my first job',
-    description: 'I started my first job as a Software Engineer at a startup.',
+    year: '2019',
+
+    description: [
+      {
+        text: 'One of my friends introduced me to Web Development. I started off by making simple HTML, CSS websites with a keen interest in design, and dynamic Javascript.',
+        title: 'Developed interest in Web Development 🚀',
+      },
+      {
+        text: 'I got an A2 diploma in Electronic with 65%. Again, a big achievement for me.',
+        title: 'Completed my secondary school 🏫',
+      },
+      {
+        text: 'One of my friends introduced me to Web Development. I started off by making simple HTML, CSS websites with a keen interest in design.',
+        title: 'Internship - Orange datacenter 🤘🏻',
+      },
+    ],
+  },
+  {
+    year: '2018',
+
+    description: [
+      {
+        text: 'I have delved into the embedded realm, harnessing microcontrollers and Arduino, crafting a world of electronic marvels',
+        title: 'Emmbeded system with microtroller 📚',
+      },
+    ],
+  },
+  {
+    year: '2016',
+
+    description: [
+      {
+        text: ' Started learning logics and basic programming fundamentals with Object Oriented Programming. C was intimidating, C++ was fun.',
+        title: 'C and C++ 📚',
+      },
+    ],
+  },
+  {
+    year: '2016',
+
+    description: [
+      {
+        text: 'I watched my first serie on my own computer, the serie was Game of Thrones.',
+        title: 'First Computer 💻 ',
+      },
+    ],
+  },
+
+  {
+    year: '2012',
+
+    description: [
+      {
+        text: 'I finished first in the promotion with 87.6% Marks. Again, a big achievement for me.',
+        title: 'Completed my primary school 🏫',
+      },
+    ],
+  },
+  {
+    year: '2001',
+
+    description: [
+      {
+        text: '',
+        title: 'An Enginner was Born in Dr Congo 👶🏼 🍼',
+      },
+    ],
   },
 ]
