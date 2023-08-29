1874a249e54d35d57355550e234bd674137821b3
benkis <benkisenge03@gmail.com>
2023-08-29 17:12:20 +0200

    Make the home page responsive

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Whatsapp from '../components/Whatsapp'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-neutral-100 dark:bg-gray-900">
-      <div className="px-0 md:px-80 pb-10 pt-5 mt-40">
+      <div className="sm:px-10 px-5 lg:px-80  pb-10 pt-5 mt-40">
         <Header />
         <Projects />
         <LifeUpdate />
